aae26dc0d9a1c37f831d7d2e385139e06c3b6863
Timofey Bobylkov <bobylkovt@gmail.com>
2022-06-10 22:49:35 +0300

    ArticulationPoints implemented

---
+++ ArticulationPoints/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import com.mathsystem.api.graph.GraphFactory;
+import com.mathsystem.api.graph.model.Graph;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) {
+        File file =  new File("C:/Users/bobyl/Desktop/univer/mlita/graphs/plugin-implementation/ArticulationPoints/src/test/graph.txt");
+
+        Graph graph = null;
+        try {
+            graph = GraphFactory.loadGraphFromFile(file);
+        } catch (FileNotFoundException ignored) {}
+        System.out.println(new ArticulationPoints().execute(graph));
+
+    }
+}
